23b833ae8a8636942f5da0243b483e0b6d4d6d61
Liz Yu <qqqq929@hotmail.com>
2022-08-06 00:43:54 +0800

    extract static data

---
+++ src/statics/filterMenu.js
@@ -0,0 +1,12 @@
+export const orderFilterConfig = [
+  { id: 1, label: '發布時間' },
+  { id: 2, label: '觀看次數' },
+  { id: 3, label: '收藏次數' },
+];
+
+export const lengthFilterConfig = [
+  { id: 1, label: '不限' },
+  { id: 2, label: '4分鐘以下' },
+  { id: 3, label: '5 - 10 分鐘' },
+  { id: 4, label: '超過 10 分鐘' },
+];
